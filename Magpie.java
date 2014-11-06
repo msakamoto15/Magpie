11805f7e9891db0748094a4c2f019069cc710bf4
msakamoto15 <msakamoto15@punahou.edu>
2014-11-06 12:22:09 -1000

    Magpie 3
    Magpie 3

--- Magpie.java
+++ Magpie.java
@@ -31,40 +31,40 @@ public class Magpie
  public String getResponse(String statement)
  {
   String response = "";
-  if (statement.indexOf("no") >= 0)
+  if (findKeyword(statement, "no") >= 0)
   {
    response = "Why so negative?";
   }
-  else if (statement.indexOf("yes") >= 0)//Added to respond to human wanting to talk
+  else if (findKeyword(statement, "yes") >= 0)//Added to respond to human wanting to talk
   {
    response = "Cool, Lets bigin.";
   }
-  else if (statement.indexOf("idk") >= 0)//Added to respond to human response
+  else if (findKeyword(statement, "idk") >= 0)//Added to respond to human response
   {
    response = "Well neither do I";//This could possibly deal with "Know" Vs "no" situations
   }
-  else if (statement.indexOf("ok") >= 0)//Added to respond to human response
+  else if (findKeyword(statement, "ok") >= 0)//Added to respond to human response
   {
    response = "Alright";
   }
-  else if (statement.indexOf("mother") >= 0
-    || statement.indexOf("father") >= 0
-    || statement.indexOf("sister") >= 0
-    || statement.indexOf("brother") >= 0)
+  else if (findKeyword(statement, "mother") >= 0
+    || findKeyword(statement, "father") >= 0
+    || findKeyword(statement, "sister") >= 0
+    || findKeyword(statement, "brother") >= 0)
   {
    response = "Tell me more about your family.";
   }
-  else if (statement.indexOf("Ted") >= 0//Computer recognises teacher's name 
-    || statement.indexOf("Doug") >= 0
-    || statement.indexOf("Kiang") >= 0
-    || statement.indexOf("Landgraf") >= 0)
+  else if (findKeyword(statement, "Ted") >= 0//Computer recognises teacher's name 
+    || findKeyword(statement, "Doug") >= 0
+    || findKeyword(statement, "Kiang") >= 0
+    || findKeyword(statement, "Landgraf") >= 0)
   {
    response = "He is a terrific teacher, truely.";///Computer responds to teacher being mentioned
   }
-   else if (statement.indexOf("Dog") >= 0//Computer recognises pet being mentioned 
-    || statement.indexOf("dog") >= 0
-    || statement.indexOf("Cat") >= 0
-    || statement.indexOf("cat") >= 0)
+   else if (findKeyword(statement, "Dog") >= 0//Computer recognises pet being mentioned 
+    || findKeyword(statement, "dog") >= 0
+    || findKeyword(statement, "Cat") >= 0
+    || findKeyword(statement, "cat") >= 0)
   {
    response = "That is a good pet to have. Tell me more.";//Computer responds to pet being mentioned
    }
@@ -78,11 +78,76 @@ public class Magpie
   }
   return response;
  }
+ private int findKeyword(String statement, String goal,
+   int startPos)
+ {
+  String phrase = statement.trim();
+  // The only change to incorporate the startPos is in
+  // the line below
+  int psn = phrase.toLowerCase().indexOf(
+    goal.toLowerCase(), startPos);
+
+  // Refinement--make sure the goal isn't part of a
+  // word
+  while (psn >= 0)
+  {
+   // Find the string of length 1 before and after
+   // the word
+   String before = " ", after = " ";
+   if (psn > 0)
+   {
+    before = phrase.substring(psn - 1, psn)
+      .toLowerCase();
+   }
+   if (psn + goal.length() < phrase.length())
+   {
+    after = phrase.substring(
+      psn + goal.length(),
+      psn + goal.length() + 1)
+      .toLowerCase();
+   }
+
+   // If before and after aren't letters, we've
+   // found the word
+   if (((before.compareTo("a") < 0) || (before
+     .compareTo("z") > 0)) // before is not a
+           // letter
+     && ((after.compareTo("a") < 0) || (after
+       .compareTo("z") > 0)))
+   {
+    return psn;
+   }
+
+   // The last position didn't work, so let's find
+   // the next, if there is one.
+   psn = phrase.indexOf(goal.toLowerCase(),
+     psn + 1);
+
+  }
+
+  return -1;
+ }
 
  /**
-  * Pick a default response to use if nothing else fits.
-  * @return a non-committal string
+  * Search for one word in phrase. The search is not case
+  * sensitive. This method will check that the given goal
+  * is not a substring of a longer string (so, for
+  * example, "I know" does not contain "no"). The search
+  * begins at the beginning of the string.
+  * 
+  * @param statement
+  *            the string to search
+  * @param goal
+  *            the string to search for
+  * @return the index of the first occurrence of goal in
+  *         statement or -1 if it's not found
   */
+ private int findKeyword(String statement, String goal)
+ {
+  return findKeyword(statement, goal, 0);
+ }
+
+ 
  private String getRandomResponse()
  {
   final int NUMBER_OF_RESPONSES = 6;//changed to choose from more responses
@@ -92,7 +157,7 @@ public class Magpie
   
   if (whichResponse == 0)
   {
-   response = "Interesting, tell me more.";
+    response = "Interesting, tell me more.";
   }
   else if (whichResponse == 1)
   {
