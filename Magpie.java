0d6d725fb60082097312f25582508845137b823d
msakamoto15 <msakamoto15@punahou.edu>
2014-11-06 19:42:22 -1000

    Activity 4
    Activity 4

--- Magpie.java
+++ Magpie.java
@@ -1,15 +1,4 @@
-/**
- * A program to carry on conversations with a human user.
- * This is the initial version that:  
- * <ul><li>
- *       Uses indexOf to find strings
- * </li><li>
- *       Handles responding to simple words and phrases 
- * </li></ul>
- * This version uses a nested if to handle default responses.
- * @author Laurie White
- * @version April 2012
- */
+
 public class Magpie
 {
  /**
@@ -68,16 +57,61 @@ public class Magpie
   {
    response = "That is a good pet to have. Tell me more.";//Computer responds to pet being mentioned
    }
-   else if (statement.trim().length() == 0)//trim method to deal with blank space responses by human
-   {
-   response = "Hmmm?...You typed blank spaces.";
-  }
-  else
+   else
   {
-   response = getRandomResponse();
+   // Look for a two word (you <something> me)
+   // pattern
+   int psn = findKeyword(statement, "you", 0);
+
+   if (psn >= 0
+     && findKeyword(statement, "me", psn) >= 0)
+   {
+    response = transformYouMeStatement(statement);
+   }
+   else
+   {
+    response = getRandomResponse();
+   }
   }
   return response;
  }
+ 
+ private String transformIWantToStatement(String statement)
+ {
+  //  Remove the final period, if there is one
+  statement = statement.trim();
+  String lastChar = statement.substring(statement
+    .length() - 1);
+  if (lastChar.equals("."))
+  {
+   statement = statement.substring(0, statement
+     .length() - 1);
+  }
+  int psn = findKeyword (statement, "I want to", 0);
+  String restOfStatement = statement.substring(psn + 9).trim();
+  return "What would it mean to " + restOfStatement + "?";
+ }
+
+
+ private String transformYouMeStatement(String statement)
+ {
+  //  Remove the final period, if there is one
+  statement = statement.trim();
+  String lastChar = statement.substring(statement
+    .length() - 1);
+  if (lastChar.equals("."))
+  {
+   statement = statement.substring(0, statement
+     .length() - 1);
+  }
+  
+  int psnOfYou = findKeyword (statement, "you", 0);
+  int psnOfMe = findKeyword (statement, "me", psnOfYou + 3);
+  
+  String restOfStatement = statement.substring(psnOfYou + 3, psnOfMe).trim();
+  return "What makes you think that I " + restOfStatement + " you?";
+ }
+ 
  private int findKeyword(String statement, String goal,
    int startPos)
  {
@@ -128,20 +162,8 @@ public class Magpie
   return -1;
  }
 
- /**
-  * Search for one word in phrase. The search is not case
-  * sensitive. This method will check that the given goal
-  * is not a substring of a longer string (so, for
-  * example, "I know" does not contain "no"). The search
-  * begins at the beginning of the string.
-  * 
-  * @param statement
-  *            the string to search
-  * @param goal
-  *            the string to search for
-  * @return the index of the first occurrence of goal in
-  *         statement or -1 if it's not found
-  */
+ 
+ 
  private int findKeyword(String statement, String goal)
  {
   return findKeyword(statement, goal, 0);
